0ede60c507b637f29a79da191ca85f2f571fdd6c
Stephen Young <90876838+DTSMY96@users.noreply.github.com>
2021-09-28 16:09:22 -0600

    Add files via upload

---
+++ print('Hello world!').py
@@ -0,0 +1,15 @@
+#print('Hello, world!')
+
+#c = input('What is your favorite color?')
+
+#print('I find it interesting that you like the color'  + c)
+
+#s = input('Which dessert do you prefer ?')
+
+#print('I prefer 3.14 over' + s)
+
+x = 5
+
+x =+ 1
+
+print(x)
